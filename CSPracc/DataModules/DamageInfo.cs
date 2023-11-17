4eb4adb29f90bc282deb1bcd2360be849939a9b4
CHR15cs <christoph_haug@t-online.de>
2023-11-17 09:39:06 +0100

    adding events

---
+++ CSPracc/DataModules/DamageInfo.cs
@@ -0,0 +1,27 @@
+﻿using CounterStrikeSharp.API.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPracc.DataModules
+{
+    public class DamageInfo
+    {
+        public CCSPlayerController? Player1;
+        protected int HitsPlayer1 { get; set; } = 0;
+        protected int DamageTakenP1 { get; set; } = 0;
+
+        public DamageInfo(CCSPlayerController player1, CCSPlayerController player2) 
+        {
+            Player1 = player1;
+        }
+        public void Update(CCSPlayerController damageTaker, int damage)
+        {
+            if(damageTaker == null) { return; }
+            HitsPlayer1++;
+            DamageTakenP1 += damage;
+        }
+    }
+}

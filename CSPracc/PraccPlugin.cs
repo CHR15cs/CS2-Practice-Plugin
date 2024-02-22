f771240c664c98003929e35fa0ce2c46480de23d
CHR15cs <christoph_haug@t-online.de>
2024-02-22 07:50:38 +0100

    V1.0.0.2

--- CSPracc/PraccPlugin.cs
+++ CSPracc/PraccPlugin.cs
@@ -44,7 +44,7 @@ public class CSPraccPlugin : BasePlugin, IPluginConfig<CSPraccConfig>
     {
         get
         {
-            return "1.0.0.1";
+            return "1.0.0.2";
         }
     }
 
